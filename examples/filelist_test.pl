1d6f5abb15e16312df483d8e0636ef5d77d826f3
elspicyjack <devnull@localhost>
2008-11-14 16:27:02 +0000

    - gutted the file, started to set up a simple random list chooser

--- examples/filelist_test.pl
+++ examples/filelist_test.pl
@@ -7,30 +7,12 @@ use warnings;
 my @playlist = <STDIN>;
 # make a copy of the playlist before we start munging it
 my @song_q = @playlist;
-foreach my $song (@playlist) {
+while ( scalar(@song_q) != 0 ) {
     chomp($song);
-            #$song =~ s/,/\\,/g;
-            #$song =~ s/ /\\ /g;
-            if ( ! -e $song ) { warn qq(song '$song' does not exist\n); }
-            # if we connect, grab data from stdin and shoot it to the server
-            my ($buff, $len);
-            warn qq(Opening file for streaming;\n'$song'\n);
-            open(MP3FILE, $song) || die qq(Can't open $song : $!);
-            while (($len = sysread(MP3FILE, $buff, 4096)) > 0) {
-    	        unless ($conn->send($buff)) {
-	                warn "Error while sending: " . $conn->get_error . "\n";
-            	    last;
-    	        } # unless ($conn->send($buff)) 
-         	    # must be careful not to send the data too fast :)
-    	        $conn->sync;
-            } # while (($len = sysread(MP3FILE, $buff, 4096)) > 0)
-            close(MP3FILE);
-            warn qq(Closing file;\n$song\n);
-        } # foreach my $song (@playlist)
-    } # while ( -e q(/tmp/streambake.die) 
-
-    # all done
-    $conn->close;
-} else {
-    warn "couldn't connect...\n";
+    my $song_q_length = scalar(@song_q);
+    my $random_song = int(rand($song_q_length));
+    my (@high_q, @low_q);
+    if ( $random_song == 0 ) {
+        @high_q = @song_q[1, $song_q_length - 1];
+    } 
 }
