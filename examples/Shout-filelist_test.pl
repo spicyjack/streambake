b6e8144d0d868221e758db861fdebd8bb1a9b611
elspicyjack <devnull@localhost>
2008-11-15 09:31:55 +0000

    - fixed Shout filelist_test to use splice to pull the song to play instead of   doing it the long way

--- examples/Shout-filelist_test.pl
+++ examples/Shout-filelist_test.pl
@@ -49,29 +49,12 @@ if ($conn->open) {
         my $song_q_length = scalar(@song_q);
         print qq(There are currently $song_q_length songs in the song Q\n);
         my $random_song = int(rand($song_q_length));
-        my (@high_q, @low_q);
-        if ( $random_song == 0 ) {
-            $current_song = $song_q[0];
-            @low_q = ();
-            @high_q = @song_q[1 .. $song_q_length - 1];
-            print qq(- first song 0 chosen\n);
-        } elsif ( $random_song == ( $song_q_length - 1 ) ) {
-            $current_song = $song_q[$song_q_length - 1];
-            @low_q = @song_q[0 .. $song_q_length - 2];
-            @high_q = ();
-            print qq(- last song ) . ($song_q_length - 1) . qq( chosen\n);
-        } else {
-            $current_song = $song_q[$random_song];
-            print qq(- random song $random_song chosen\n);
-            @low_q = @song_q[0 .. $random_song - 1];
-            #print qq(- Selected ) . scalar(@low_q) . qq( into low queue\n);
-            @high_q = @song_q[($random_song + 1) .. $song_q_length - 1];
-            #print qq(- Selected ) . scalar(@high_q) . qq( into high queue\n);
-        } # if ( $random_song == 0 )
+        $current_song = splice(@song_q, $random_song, 1);
         chomp($current_song);
         print qq(Current song is $current_song\n);
         if ( ! -e $current_song ) { 
             warn qq(File '$current_song' doesn't exist\n); 
+            next;
         } # if ( ! -e $current_song ) 
         # if we connect, grab data from stdin and shoot it to the server
         my ($buff, $len);
@@ -88,8 +71,6 @@ if ($conn->open) {
         close(MP3FILE);
         warn qq(Closing file;\n$current_song\n);
 
-        # combine the leftovers back into the song_q again
-        @song_q = ( @low_q, @high_q );
         if ( scalar(@song_q) == 0 ) {
             print qq(Reloading song queue;\n);
             @song_q = @playlist;
