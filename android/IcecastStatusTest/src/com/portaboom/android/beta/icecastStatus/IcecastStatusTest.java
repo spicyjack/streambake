9029fddb552ce4dea20d0521827614fc8ccdd953
elspicyjack <devnull@localhost>
2009-03-11 07:17:45 +0000

    - simple icecast test application works and downloads the current playlist

--- android/IcecastStatusTest/src/com/portaboom/android/beta/icecastStatus/IcecastStatusTest.java
+++ android/IcecastStatusTest/src/com/portaboom/android/beta/icecastStatus/IcecastStatusTest.java
@@ -1,37 +1,43 @@
 package com.portaboom.android.beta.icecastStatus;
 
-// apache imports
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicResponseHandler;
+// java
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 // android imports
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class IcecastStatusTest extends Activity {
     /** Called when the activity is first created. */
-	private HttpClient client;
-	
-	String url = "http://stream.portaboom.com:7767/status2.xsl";
+	static final String TAG = "IcecastStatusTest";
+	String statPage = "http://stream.portaboom.com:7767/simple.xsl";
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        HttpGet getMethod = new HttpGet(url);
-        String rb = "";
-        try {
-        	ResponseHandler<String> rh = new BasicResponseHandler();
-        	rb = client.execute(getMethod, rh);
+		super.onCreate(savedInstanceState);
+        String httpRequest = statPage, line = "";
+        Log.v(TAG, "starting onCreate; statPage is " + statPage);
+		try {
+	        URL webURL = new URL(statPage);
+	        BufferedReader is = new BufferedReader(
+	            new InputStreamReader(webURL.openStream()));
+
+			while ((line = is.readLine()) != null) {
+	            httpRequest = httpRequest + line;
+	        }
+	        is.close();
         } catch (Throwable t) {
         	Toast 
         		.makeText(this, "Request failed: " + t.toString(), 4000);
         		//.show();
         }
         TextView tv = new TextView(this);
-        tv.setText(rb);
+        tv.setText(httpRequest);
         setContentView(tv);
         //Object o = null;
         //o.toString();
